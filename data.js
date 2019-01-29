571df78eb1dcf50c7ca054b7f0d717065ebbec2d
qisheng <qisheng@hqins.cn>
2019-01-29 14:14:38 +0800

    shdjk

---
+++ data.js
@@ -0,0 +1,50 @@
+var questionData=[
+    {"order":"0","title":"年夜饭，第一筷子你会先吃什么？",
+        "options":[{"order":"0","value":"A 菜"},
+            {"order":"1","value":"B 肉"},
+            {"order":"2","value":"C 米饭"},
+            {"order":"3","value":"D 第一筷子轮不到我吃"}
+        ]},
+    {"order":"1","title":"你结婚了吗？",
+        "options":[{"order":"0","value":"A 已婚"},
+            {"order":"1","value":"B 未婚"},
+            {"order":"2","value":"C 离异"},
+            {"order":"3","value":"D 管你P事"}
+        ]},
+    {"order":"2","title":"孩子学习怎么样？",
+        "options":[{"order":"0","value":"A ojbk"},
+            {"order":"1","value":"B 让人操心"},
+            {"order":"2","value":"C 管你P事"}
+        ]},
+    {"order":"3","title":"找对象了吗？",
+        "options":[{"order":"0","value":"A 交往中"},
+            {"order":"1","value":"B 让人没时间操心"},
+            {"order":"2","value":"C 急有用吗？"}
+        ]},
+    {"order":"4","title":"计划要2胎吗？",
+        "options":[{"order":"0","value":"A 没计划"},
+            {"order":"1","value":"B 一个就够了"},
+            {"order":"2","value":"C 管你P事"}
+        ]},
+    {"order":"5","title":"春节最大的心愿",
+        "options":[{"order":"0","value":"A 抢多多的红包"},
+            {"order":"1","value":"B 多陪陪父母"},
+            {"order":"2","value":"C 浪的有声有色"},
+            {"order":"3","value":"D 以上皆是"}
+        ]},
+    {"order":"6","title":"什么时候结婚？",
+        "options":[{"order":"0","value":"A 明年"},
+            {"order":"1","value":"B 不着急"},
+            {"order":"2","value":"C 管你P事"}
+        ]},
+    {"order":"7","title":"对象有车有房吗？",
+        "options":[{"order":"0","value":"A 有房"},
+            {"order":"1","value":"B 都有"},
+            {"order":"2","value":"C 没有你给买吗？"}
+        ]},
+    {"order":"8","title":"你有小孩吗？",
+        "options":[{"order":"0","value":"A 1个"},
+            {"order":"1","value":"B 2个"},
+            {"order":"2","value":"C 管你P事"}
+        ]}
+];
