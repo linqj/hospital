c124aa6b662447cc7446eb80fe77d25fdd2860c9
System Administrator <root@linqiaojuan.local>
2019-02-12 17:37:05 +0800

    埋点

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-// 点击立即诊断
 var app = document.getElementById("app");
 var firstPage = document.getElementsByClassName("first-page")[0];
 var secondPage = document.getElementsByClassName("second-page")[0];
@@ -6,37 +5,23 @@ var threePage = document.getElementsByClassName("three-page")[0];
 var fourPage = document.getElementsByClassName("four-page")[0];
 var handMove = document.getElementsByClassName("hand-move")[0];
 var handMoveImg = document.getElementsByClassName("hand-move-img")[0];
-var fourPageImg = document.getElementsByClassName(
-  "four-page-content-img"
-)[0];
-var firstPageInput = document.getElementsByClassName(
-  "first-page-input"
-)[0];
-var secondPageTitle = document.getElementsByClassName(
-  "second-page-title"
-)[0];
-var secondPageContents = document.getElementsByClassName(
-  "second-page-content"
-);
-// var pageNum = 0 || sessionStorage.getItem("pageNum");
-var pageNum = 0; //当前是第几题
-var nameValue = null; //用户输入的名称
-function blurInput(){
-  //输入框失去焦点，
-      //把页面滑动到顶端
-      document.body.scrollTop =0;
-     document.documentElement.scrollTop = 0;
+var fourPageImg = document.getElementsByClassName("four-page-content-img")[0];
+var firstPageInput = document.getElementsByClassName("first-page-input")[0];
+var secondPageTitle = document.getElementsByClassName("second-page-title")[0];
+var secondPageContents = document.getElementsByClassName("second-page-content");
+var pageNum = 0;
+var nameValue = null; 
+function blurInput() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 function toInsure() {
   nameValue = firstPageInput.value;
-  //判断是否已经输入了值
   if (!nameValue) {
     alert("请输入您的姓名");
   } else {
-    //把页面滑动到顶端
-      document.body.scrollTop =0;
-     document.documentElement.scrollTop = 0;
-    //点击立即诊断，换背景图片隐藏首页的内容
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
     app.style.backgroundImage = "url('./images/background2.jpg')";
     firstPage.style.display = "none";
     secondPage.style.display = "block";
@@ -45,13 +30,12 @@ function toInsure() {
   }
   this.operateOption(pageNum);
 }
-//根据第几题展示内容
 function operateOption(num) {
   var that = this;
   var option = questionData.find(item => {
     return item.order == num;
   });
-  var isUnNormal = false; //是否选择了关你屁事
+  var isUnNormal = false; 
   secondPageTitle.innerHTML = option.title;
   option.options.forEach((item, index) => {
     var parentNode = secondPageContents[index];
@@ -75,15 +59,11 @@ function operateOption(num) {
           if (num == "0") {
             pageNum += 1;
           } else if (num == "1") {
-            // merrageCode = i;
-            //第二题判断是选择的那一道题
             if (i == "0") {
               pageNum = 8;
-              //选择已婚
             } else if (i == "1" || i == "2") {
               pageNum = 3;
             } else {
-              //选择了管你屁事
               isUnNormal = true;
               that.dealNoCare(num);
             }
@@ -93,13 +73,11 @@ function operateOption(num) {
             } else if (i == "1") {
               pageNum = 2;
             } else {
-              //选择了管你屁事
               isUnNormal = true;
               that.dealNoCare(num);
             }
           } else if (num == 4) {
             if (i == 2) {
-              //选择了管你屁事
               isUnNormal = true;
               that.dealNoCare(num);
             } else {
@@ -107,7 +85,6 @@ function operateOption(num) {
             }
           } else if (num == 2) {
             if (i == 2) {
-              //选择了管你屁事
               isUnNormal = true;
               that.dealNoCare(num);
             } else {
@@ -143,44 +120,37 @@ function operateOption(num) {
     }
   }
 }
-//处理关你屁事的选项
 function dealNoCare(num) {
   handMove.style.display = "block";
   handMoveImg.style.animation = "mymove 0.1s forwards";
-  if(num=='1'){
+  if (num == "1") {
     this.setNestTep(3);
-  }else if(num=='8'){
-    this.setNestTep(4);
-  }else if(num=='2'||num=='6'){
+  } else if (num == "8") {
     this.setNestTep(5);
-  }else if(num=='4'){
+  } else if (num == "2" || num == "6") {
+    this.setNestTep(5);
+  } else if (num == "4") {
     this.setNestTep(2);
   }
 }
-function setNestTep(index){
+function setNestTep(index) {
   setTimeout(function() {
-    pageNum=index;
+    pageNum = index;
     this.operateOption(pageNum);
     handMove.style.display = "none";
     handMoveImg.style.width = "10%";
     handMoveImg.style.animation = "";
   }, 1000);
 }
-//最后结果页
 function dealResult() {
   handMove.style.display = "none";
-  var customerName = document.getElementsByClassName(
-    "customer-item-name"
-  )[0];
-  var signDate = document.getElementsByClassName(
-    "four-page-sign-date"
-  )[0];
+  var customerName = document.getElementsByClassName("customer-item-name")[0];
+  var signDate = document.getElementsByClassName("four-page-sign-date")[0];
   // // 首先进入loading页面
   app.style.backgroundImage = "url('./images/background3.jpg')";
   secondPage.style.display = "none";
   threePage.style.display = "block";
 
-  //loading页面显示2秒后进入结果页面
   setTimeout(function() {
     app.style.backgroundImage = "url('./images/background4.jpg')";
     threePage.style.display = "none";
@@ -188,40 +158,35 @@ function dealResult() {
     customerName.innerHTML = "姓名：" + nameValue;
     signDate.innerHTML = "日期：" + this.getNowDate();
     fourPageImg.style.display = "block";
-    // html转化为canvas生成图片
     this.html2Canvas();
   }, 3000);
 }
 
 function html2Canvas() {
-  const shareContent = fourPage; // 需要绘制的部分的 (原生）dom 对象 ，注意容器的宽度不要使用百分比，使用固定宽度，避免缩放问题
-  const width = shareContent.offsetWidth; // 获取(原生）dom 宽度
-  const height = shareContent.offsetHeight; // 获取(原生）dom 高
-  const offsetTop = shareContent.offsetTop; // 元素距离顶部的偏移量
-  const canvas = document.createElement("canvas"); // 创建canvas 对象
+  const shareContent = fourPage; 
+  const width = shareContent.offsetWidth;
+  const height = shareContent.offsetHeight; 
+  const offsetTop = shareContent.offsetTop;
+  const canvas = document.createElement("canvas"); 
   const context = canvas.getContext("2d");
-  const scaleBy = getPixelRatio(context); // 获取像素密度的方法 (也可以采用自定义缩放比例)
-  canvas.width = width * scaleBy; // 这里 由于绘制的dom 为固定宽度，居中，所以没有偏移
-  canvas.height = (height + offsetTop) * scaleBy; // 注意高度问题，由于顶部有个距离所以要加上顶部的距离，解决图像高度偏移问题
+  const scaleBy = getPixelRatio(context); 
+  canvas.width = width * scaleBy; 
+  canvas.height = (height + offsetTop) * scaleBy; 
   context.scale(scaleBy, scaleBy);
   const opts = {
-    // allowTaint: true, // 允许加载跨域的图片 （使用这个会报错）
-    useCORS: true, // 允许加载跨域图片
-    tainttest: true, // 检测每张图片都已经加载完成
-    scale: scaleBy, // 添加的scale 参数
-    canvas: canvas, // 自定义 canvas
-    // logging: true, // 日志开关，发布的时候记得改成false
-    width: width, // dom 原始宽度
-    height: height // dom 原始高度
+    useCORS: true,
+    tainttest: true, 
+    scale: scaleBy, 
+    canvas: canvas, 
+    width: width, 
+    height: height 
   };
   html2canvas(shareContent, opts).then(function(canvas) {
     var context = canvas.getContext("2d");
-    // 【重要】关闭抗锯齿
     context.mozImageSmoothingEnabled = false;
     context.webkitImageSmoothingEnabled = false;
     context.msImageSmoothingEnabled = false;
     context.imageSmoothingEnabled = false;
-    //添加属性
     canvas.setAttribute("id", "thecanvas");
     fourPage.innerHTML = "";
     fourPage.appendChild(canvas);
@@ -234,7 +199,6 @@ function html2Canvas() {
   });
 }
 function getPixelRatio(context) {
-  // 获取设备的pixel ratio
   const backingStore =
     context.backingStorePixelRatio ||
     context.webkitBackingStorePixelRatio ||
@@ -248,9 +212,9 @@ function getPixelRatio(context) {
 
 function getNowDate() {
   var now = new Date();
-  var year = now.getFullYear(); //年
-  var month = now.getMonth() + 1; //月
-  var day = now.getDate(); //日
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1; 
+  var day = now.getDate();
   var clock = year + "年";
   if (month < 10) {
     clock += "0";
